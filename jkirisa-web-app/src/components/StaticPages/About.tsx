94b3e9620e4404a3707be5d6570907550c6262c1
King Jude <jcristobal2391@gmail.com>
2025-10-09 19:21:55 -0400

    Add static pages components

---
+++ jkirisa-web-app/src/components/StaticPages/About.tsx
@@ -0,0 +1,22 @@
+export const About = () => {
+    return(
+        <div className="main-body-section">
+            <div className="container">
+                <div className="block-title">
+                    <h2>About Me!</h2>
+                </div>
+                <div className="about-content">
+                    <p>
+                        lorem ipsum, a lot of words
+                    </p>
+                    <p>
+                        lorem ipsum, al ot of wrodas two
+                    </p>
+                    <p>
+                        lorem ipsum, et. el
+                    </p>
+                </div>
+            </div>
+        </div>
+    );
+};
