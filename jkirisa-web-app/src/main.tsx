80cf0bef4e3529117d392a823a167fd1d8a69b7a
King Jude <jcristobal2391@gmail.com>
2025-10-09 22:42:27 -0400

    revert

--- jkirisa-web-app/src/main.tsx
+++ jkirisa-web-app/src/main.tsx
@@ -1,12 +1,10 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'src/assets/scss/index.scss';
-import 'antd/dist/antd.css';
-import { App } from './App/App';
+import { StrictMode } from 'react'
+import { createRoot } from 'react-dom/client'
+import './index.css'
+import App from './App.tsx'
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App></App>
-  </React.StrictMode>,
-  document.getElementById('root'),
-);
+createRoot(document.getElementById('root')!).render(
+  <StrictMode>
+    <App />
+  </StrictMode>,
+)
