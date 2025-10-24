be0c3752f0c0ad1fa462da86e440b2ed866c8ba7
jcristobal2391 <54490463+jcristobal2391@users.noreply.github.com>
2025-10-24 11:56:19 -0400

    Feature/setup gha (#7)
    * Adding GHA
    
    * Adding testing branch
    
    * Add AWS region
    
    * add registries
    
    * Add credentials login
    
    * remove registries
    
    * delete starter workflow
    
    * change dir
    
    * Remove test push
    
    * added example pdfs
    
    * Update about page

--- jkirisa-web-app/src/components/StaticPages/About.tsx
+++ jkirisa-web-app/src/components/StaticPages/About.tsx
@@ -7,13 +7,18 @@ export const About = () => {
                 </div>
                 <div className="about-content">
                     <p>
-                        lorem ipsum, a lot of words
+                        When I was a boy, I read a lot of fantasy books. With Dragons and magic, romance and swords,
+                        dark, dark nights, and the glorious dawn. And like young boys are wont to do, I daydreamed 
+                        of cool things. Of a hero who would never, ever let me down. I wanted to write about him 
+                        and share cool ideas with the world.
                     </p>
                     <p>
-                        lorem ipsum, al ot of wrodas two
+                        That never happened. Instead, I played it safe. Went to college at a good school. Worked hard 
+                        on my degree. Got a good job, with good hours, good pay, and good people. Everything I ever 
+                        wanted. Most of what I wanted.
                     </p>
                     <p>
-                        lorem ipsum, et. el
+                        Enter Stories! Everyone's got a childhood dream they never gave up on. And this is mine.
                     </p>
                 </div>
             </div>
