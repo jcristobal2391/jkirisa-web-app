1ef7859570f8eeac00bd39e9da1ad2bff4d59aa0
King Jude <jcristobal2391@gmail.com>
2025-10-25 16:50:06 -0400

    Add pdf reader component

--- jkirisa-web-app/src/main.tsx
+++ jkirisa-web-app/src/main.tsx
@@ -5,6 +5,9 @@ import { store } from 'src/store'
 import 'src/assets/scss/index.scss'
 import 'antd/dist/reset.css'
 import { App } from 'src/App/App'
+import { pdfjs } from 'react-pdf';
+
+pdfjs.GlobalWorkerOptions.workerSrc = '../public/pdf.worker.min.js';
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
